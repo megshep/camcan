888fa3e50361c71b84a8d912f257f9bc36520f5e
Meg Sheppard <142318637+megshep@users.noreply.github.com>
2026-07-27 14:29:35 +0100

    Create counting_participants_images_lifespan.R
    This code counts how many scans are associated with each age bin across the lifespan

---
+++ counting_participants_images_lifespan.R
@@ -0,0 +1,27 @@
+library(dplyr)
+library(tidyr)
+library(rstatix)
+
+df <- read.xlsx('participants.xlsx')
+
+#ses3 ages are character format, so convert first
+df$Age_p3 <- as.numeric(df$Age_p3)
+
+# Convert to long format
+ages_long <- df %>% pivot_longer( cols = starts_with("Age"), names_to = "Timepoint",values_to = "Age") %>%
+  filter(!is.na(Age))
+
+# Define age bins
+breaks <- c(20, 30, 40, 50, 60, 70, 80, 90, 100)
+labels <- c("20-30", "3-40", "40-50", "50-60",
+            "60-70", "70-80", "80-90", "90-100")
+
+# Count scans in each age range
+age_counts <- ages_long %>%  mutate( AgeRange = cut(Age,
+      breaks = breaks,
+      labels = labels,
+      right = FALSE,      # 20–<30, 30–<40, etc.
+      include.lowest = TRUE)) %>%
+  count(AgeRange)
+
+print(age_counts)
